1020ee711303098f2778ecf6bd8eb8ecdb82aa80
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-06-06 22:54:33 +0600

    react router and intial set done

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-red-500 text-3xl">this is home</h2>
+            <button className="btn btn-active btn-primary">Primary</button>
+
+        </div>
+    );
+};
+
+export default Home;
